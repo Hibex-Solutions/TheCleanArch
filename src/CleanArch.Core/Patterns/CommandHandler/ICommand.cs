b9508a60a8bce274bb6ccae4d81295f422c9661f
Erlimar Silva Campos <erlimar@hibex.co>
2023-11-05 01:17:12 -0300

    Atualiza cabeçalhos e autor do projeto

--- src/CleanArch.Core/Patterns/CommandHandler/ICommand.cs
+++ src/CleanArch.Core/Patterns/CommandHandler/ICommand.cs
@@ -1,5 +1,5 @@
-// Copyright (c) E5R Development Team. All rights reserved.
-// This file is a part of E5R CleanArch.
+// Copyright (c) Hibex Solutions. All rights reserved.
+// This file is a part of CleanArch.
 // Licensed under the Apache version 2.0: LICENSE file.
 
 namespace CleanArch.Core;
