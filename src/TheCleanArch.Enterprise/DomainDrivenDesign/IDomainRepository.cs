244c5d084dba46f73d52e6a3eceec2d26dfc1511
Erlimar Silva Campos <erlimar@hibex.co>
2024-05-30 17:45:26 -0300

    Cria marcadores para objetos de domínio: factory, repository e service

---
+++ src/TheCleanArch.Enterprise/DomainDrivenDesign/IDomainRepository.cs
@@ -0,0 +1,10 @@
+// Copyright (c) The Clean Arch Project. All rights reserved.
+// This file is a part of TheCleanArch.
+// Licensed under the Apache version 2.0: LICENSE file.
+
+namespace TheCleanArch.Enterprise.DomainDrivenDesign;
+
+/// <summary>
+/// Marca um repositório de domínio
+/// </summary>
+public interface IDomainRepository { }
