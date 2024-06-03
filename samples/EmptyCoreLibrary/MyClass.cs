a07100cc28aa9ed554dd7ed7fa24934a4492c224
Erlimar Silva Campos <erlimar@hibex.co>
2024-06-03 14:11:59 -0300

    Adiciona exemplo de biblioteca com TheCleanArch.Core

---
+++ samples/EmptyCoreLibrary/MyClass.cs
@@ -0,0 +1,23 @@
+// Copyright (c) The Clean Arch Project. All rights reserved.
+// This file is a part of TheCleanArch.
+// Licensed under the Apache version 2.0: LICENSE file.
+
+using TheCleanArch.Core.Patterns.GuardClauses;
+
+namespace EmptyCoreLibrary;
+
+/// <summary>
+/// Classe de exemplo na biblioteca
+/// </summary>
+/// <remarks>
+/// Sempre documente suas classes. Essa é a documentação de desenvolvimento principal.
+/// </remarks>
+public class MyClass
+{
+    private readonly object _instance;
+
+    public MyClass(object instance)
+    {
+        _instance = Guard.NotNullArgument(instance, nameof(instance));
+    }
+}
