299a7997a03cde07b7c1648a1523ad470762dd7e
Erlimar Silva Campos <erlimar@hibex.co>
2024-11-13 18:58:10 +0000

    [fix-style] Formata estilo de código

--- test/TheCleanArch.EnterpriseTests/Usings.cs
+++ test/TheCleanArch.EnterpriseTests/Usings.cs
@@ -3,8 +3,8 @@
 // Licensed under the Apache version 2.0: LICENSE file.
 
 global using System;
-global using System.Diagnostics.CodeAnalysis;
 global using System.Collections.Generic;
+global using System.Diagnostics.CodeAnalysis;
 global using System.Linq;
 
 global using Xunit;
