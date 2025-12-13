b6341f3a115a6201bcfd7e23dce9218a630c90d5
Erlimar Silva Campos <erlimar@hibex.co>
2025-12-12 22:48:20 -0300

    Atualiza exemplo EmptyCoreLibrary para .NET 10

--- samples/EmptyCoreLibrary/AssemblyInfo.cs
+++ samples/EmptyCoreLibrary/AssemblyInfo.cs
@@ -1,7 +1,3 @@
-// Copyright (c) The Clean Arch Project. All rights reserved.
-// This file is a part of TheCleanArch.
-// Licensed under the Apache version 2.0: LICENSE file.
-
 using static TheCleanArch.Core.ArchLayerId;
 
 // TODO: Defina aqui a qual camada da arquitetura pertence essa biblioteca
